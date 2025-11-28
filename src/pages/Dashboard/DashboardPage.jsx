a1ca4d7919e888c2b5d3252303411fce54fe22f2
Stephanie Mei <smei826@gmail.com>
2025-11-28 16:50:54 -0500

    basic file structures added

---
+++ src/pages/Dashboard/DashboardPage.jsx
@@ -0,0 +1,15 @@
+// Main calendar view (month grid)
+// Top controls: previous/next month, go to today, month/year display
+// buttons:
+// 'create' new event
+// 'event overview' --> page with past and upcoming event
+// 'clasp' --> hide or review sidebar
+// 'arrow' navigate to different months
+// 'delete' event
+// 'edit' event
+// 'exit' event card
+// Cards on the right/side/top:
+// NotesCard
+// LabelCard
+// mini calendar
+// event card view
---
+++ src/pages/EventOverview/EventOverviewPage.jsx
@@ -0,0 +1,6 @@
+// Show all events in date order
+// Past events section
+// Upcoming events section
+// Each event rendered as EventCard (rectangle)
+// Small calendar (for context) somewhere in the layout
+// Buttons to edit/delete events
