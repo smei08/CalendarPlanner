e2f9a8c5277812da59b3aa28ec4639501b4888a2
Stephanie Mei <smei826@gmail.com>
2025-11-28 18:08:26 -0500

    appLayOut v1

---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,34 @@
+import Navbar from "../components/navigation/Navbar";
+import MiniCalenderCard from "../components/calendar/MiniCalendar";
+import LabelCard from "../components/notes/LabelCard";
+import NotesCard from "../components/notes/NotesCard";
+import { useState } from "react";
+
+export default function AppLayout({ children }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
+  return (
+    <div className="app-layout">
+      {/* top header */}
+      <header className="top-header">
+        <Navbar onToggleExpand={toggleExpand} />
+      </header>
+
+      {/* header cards (when unclasped) */}
+      {isExpanded && (
+        <div className="header-cards">
+          <MiniCalenderCard />
+          <LabelCard />
+          <NotesCard />
+        </div>
+      )}
+
+      {/* main calendar content (page goes here) */}
+      <main className="calendar-content">{children}</main>
+    </div>
+  );
+}
