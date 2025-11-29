17c4d3510d3a1f8b18e6fcd1f90e0e24873b85c7
Stephanie Mei <smei826@gmail.com>
2025-11-28 19:05:35 -0500

    navbar basics setup

---
+++ src/components/navigation/Navbar.jsx
@@ -0,0 +1,29 @@
+import IconButton from "../ui/IconButton";
+import Button from "../ui/Button";
+import MonthSwitcher from "../navigation/MonthSwitcher";
+import Avatar from "../ui/Avatar";
+
+export default function Navbar({ onToggleExpand }) {
+  return (
+    <div className="nav-bar">
+      {/* left section: clasp and create btn */}
+      <div className="nav-left">
+        <IconButton variant="clasp" onClick={onToggleExpand} />
+        <IconButton variant="add" />
+      </div>
+
+      {/* middle section: (today pill/butn) and (<MonthSwitcher />, <> + month/year) */}
+      <div className="nav-middle">
+        <Button variant="pill">Today</Button>
+        <MonthSwitcher />
+      </div>
+
+      {/* right section: calender icon, eventoverview btn, profile pic */}
+      <div className="nav-right">
+        <IconButton variant="calendar" />
+        <IconButton variant="eventOverview" />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
