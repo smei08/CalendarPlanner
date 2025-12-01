ad439de48f9b731b57e2aed5e203f1a31bd96d0c
Stephanie Mei <smei826@gmail.com>
2025-12-01 12:40:55 -0500

    calendarGrid skeleton complete

--- src/components/calendar/CalendarGrid.jsx
+++ src/components/calendar/CalendarGrid.jsx
@@ -1,3 +1,32 @@
 export default function CalendarGrid() {
-  return <div>Calendar grid</div>;
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const days = [];
+
+  for (let i = 1; i <= 42; i++) {
+    days.push(i);
+  }
+
+  return (
+    <div className="calendar-grid-container">
+      <div className="weekday-row">
+        {weekdays.map((weekday) => (
+          <div key={weekday}>{weekday}</div>
+        ))}
+      </div>
+      <div className="day-cells-grid">
+        {days.map((day) => (
+          <div key={day}>{day}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
