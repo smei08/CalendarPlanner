8aac2df2744de22ab12eea751fbb9872d3f2016d
Stephanie Mei <smei826@gmail.com>
2025-12-05 13:56:27 -0500

    useState add for eventform

--- src/components/events/EventForm/EventForm.jsx
+++ src/components/events/EventForm/EventForm.jsx
@@ -1,37 +1,60 @@
-// import IconButton from "../../ui/IconButton";
+import { useState } from "react";
 import "./eventForm.css";
 
 export default function EventForm({ onClose }) {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [label, setLabel] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div className="form-container">
       <div className="form-header">
         <h2>Plan your day</h2>
-        {/* X button */}
+
         <button type="button" className="form-close-button" onClick={onClose}>
           ×
         </button>
       </div>
 
       <form className="form-body">
+        {/* TITLE */}
         <input
           className="input title"
           name="title"
           placeholder="Add Title"
           type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
 
+        {/* DATE + TIME */}
         <div className="form-row">
           <label>
             Date
-            <input className="input" type="date" name="date" />
+            <input
+              className="input"
+              type="date"
+              name="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
           </label>
 
           <label>
             Time
-            <input className="input" type="time" name="time" />
+            <input
+              className="input"
+              type="time"
+              name="time"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+            />
           </label>
         </div>
 
+        {/* LABEL */}
         <label>
           Label
           <input
@@ -39,18 +62,24 @@ export default function EventForm({ onClose }) {
             name="label"
             placeholder="e.g. Work, Birthday, Study"
             type="text"
+            value={label}
+            onChange={(e) => setLabel(e.target.value)}
           />
         </label>
 
+        {/* DESCRIPTION */}
         <label>
           Description
           <textarea
             className="textarea"
             name="description"
             placeholder="Add optional notes"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
         </label>
 
+        {/* BUTTONS */}
         <div className="form-actions">
           <button type="button" onClick={onClose}>
             Cancel
