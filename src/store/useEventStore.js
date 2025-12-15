e0e88adbe3b014392ac93463d4ab58cd2af2089d
Stephanie Mei <smei826@gmail.com>
2025-12-15 12:44:14 -0500

    local storage added

--- src/store/useEventStore.js
+++ src/store/useEventStore.js
@@ -1,76 +1,80 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useEventStore = create((set, get) => ({
-  eventByDate: {},
-  createEvent: (eventInput) => {
-    const { title, date, time, label, description } = eventInput;
-    let id;
-    if (typeof crypto !== "undefined" && crypto.randomUUID) {
-      id = crypto.randomUUID();
-    } else {
-      id = `evt-${Date.now}-${Math.random().toString(16).slice(2)}`;
-    }
-    const dateKey = date;
-
-    const newEvent = {
-      id,
-      title: title.trim(),
-      dateKey,
-      time,
-      label: label.trim(),
-      description,
-      createdAt: new Date().toISOString(),
-    };
-
-    set((state) => {
-      const existingForDate = state.eventByDate[dateKey] || [];
-      return {
-        eventByDate: {
-          ...state.eventByDate,
-          [dateKey]: [...existingForDate, newEvent],
-        },
-      };
-    });
-    return newEvent;
-  },
-
-  updateEvent: (dateKey, eventId, eventUpdate) => {
-    set((state) => {
-      console.log("old: ", state.eventByDate);
-      const existingForDate = state.eventByDate[dateKey] || [];
-
-      const updatedDate = existingForDate.map((event) => {
-        return event.id === eventId ? { ...event, ...eventUpdate } : event;
-      });
-
-      return {
-        eventByDate: {
-          ...state.eventByDate,
-          [dateKey]: updatedDate,
-        },
-      };
-    });
-  },
-
-  deleteEvent: (dateKey, eventId) => {
-    set((state) => {
-      const existingForDate = state.eventByDate[dateKey] || [];
-
-      const updatedDate = existingForDate.filter((event) => {
-        return event.id !== eventId;
-      });
-
-      const updatedDateCopy = { ...state.eventByDate };
-
-      if (updatedDate.length === 0) {
-        delete updatedDateCopy[dateKey];
+export const useEventStore = create(
+  persist((set, get) => ({
+    eventByDate: {},
+    createEvent: (eventInput) => {
+      const { title, date, time, label, description } = eventInput;
+      let id;
+      if (typeof crypto !== "undefined" && crypto.randomUUID) {
+        id = crypto.randomUUID();
       } else {
-        updatedDateCopy[dateKey] = updatedDate;
+        id = `evt-${Date.now}-${Math.random().toString(16).slice(2)}`;
       }
+      const dateKey = date;
 
-      return {
-        eventByDate: updatedDateCopy,
+      const newEvent = {
+        id,
+        title: title.trim(),
+        dateKey,
+        time,
+        label: label.trim(),
+        description,
+        createdAt: new Date().toISOString(),
       };
-    });
-  },
-}));
+
+      set((state) => {
+        const existingForDate = state.eventByDate[dateKey] || [];
+        return {
+          eventByDate: {
+            ...state.eventByDate,
+            [dateKey]: [...existingForDate, newEvent],
+          },
+        };
+      });
+      return newEvent;
+    },
+
+    updateEvent: (dateKey, eventId, eventUpdate) => {
+      set((state) => {
+        console.log("old: ", state.eventByDate);
+        const existingForDate = state.eventByDate[dateKey] || [];
+
+        const updatedDate = existingForDate.map((event) => {
+          return event.id === eventId ? { ...event, ...eventUpdate } : event;
+        });
+
+        return {
+          eventByDate: {
+            ...state.eventByDate,
+            [dateKey]: updatedDate,
+          },
+        };
+      });
+    },
+
+    deleteEvent: (dateKey, eventId) => {
+      set((state) => {
+        const existingForDate = state.eventByDate[dateKey] || [];
+
+        const updatedDate = existingForDate.filter((event) => {
+          return event.id !== eventId;
+        });
+
+        const updatedDateCopy = { ...state.eventByDate };
+
+        if (updatedDate.length === 0) {
+          delete updatedDateCopy[dateKey];
+        } else {
+          updatedDateCopy[dateKey] = updatedDate;
+        }
+
+        return {
+          eventByDate: updatedDateCopy,
+        };
+      });
+    },
+  })),
+  { name: "calendarplanner-events-v1" }
+);
