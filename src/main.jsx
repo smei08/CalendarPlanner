7126e922932f053c5f146839f678515d279bdf32
Stephanie Mei <smei826@gmail.com>
2025-12-29 13:59:51 -0500

    updated route to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
+import EventOverviewPage from "./pages/EventOverview/EventOverviewPage";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Navigate to="/calendar" replace />} />
+        <Route path="/calendar" element={<DashboardPage />} />
+        <Route path="/events" element={<EventOverviewPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
